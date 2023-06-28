663fe2091519678ddbee5560a978d4bb8168fc6e
Lizbeth Freire <lizbeth.freire@epn.edu.ec>
2023-06-28 17:44:45 -0500

    Use State y Use effect
    actualizar variables - actualizar arreglos

--- 03.1-react/my-app/app/c_componentes/CComponente.tsx
+++ 03.1-react/my-app/app/c_componentes/CComponente.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 
 export type PropiedadesComponente ={
     url: string;
@@ -7,20 +8,45 @@ export type PropiedadesComponente ={
 export default function CComponente(
     props: PropiedadesComponente
 ) {
-    const {url, iteraciones, mostrar} = props;
-    const contenidoAdicional = () => {
-        if(mostrar){
-            return <p>Mostrar</p>
-        }
-        return <p>Ocultar</p>
-    }
+    // const url = props.url
+    // const iteraciones = props.iteraciones
+    // const mostrar = props.mostrar
+    // const numeroUno = arreglo[0]
+    // const numeroDos = arreglo[1]
+    const {url, iteraciones, mostrar} = props, arreglo = [0, 1], [numeroUno, numeroDos] = arreglo,
+        contenidoAdicional = () => {
+            if (mostrar) {
+                return <p>Mostrar</p>
+            }
+            return <p>Ocultar</p>
+        };
+    const objeto = {}
+    //useState
+    const [iteracionLocal, setIteracionLocal] = useState(
+        iteraciones // valor de la variable
+    ) //click al boton, cambiar al classname de bg-yelow-500
+    // a bg-red-500 y cuando vuelva a dar click de nuevo cambiar a bg-yellow
+    const estilos = ["yellow", "red"]
+    const [estilosParrafo, setEstilosParrafo] = useState(
+        estilos[0]
+    )
+
     return(
-        <>
+        <div className="border border-solid border-black p-3 m-2">
             <a target="_blank" href={url}>Ir a url</a>
-            <p>Iteracion: {iteraciones}</p>
+            <p className={`bg-${estilosParrafo}-500`}>Iteracion: {iteraciones} {iteracionLocal}</p>
             <p>Mostrar: {mostrar}</p>
             {contenidoAdicional()}
-        </>
+            {mostrar && <p>Mostrar Rapido</p>}
+
+            <button className="border border-solid border-black bg-blue-500" onClick={
+                (event) => {
+                    setEstilosParrafo(estilosParrafo == estilos[0]? estilos[1]: estilos[0])
+                    setIteracionLocal(iteracionLocal+1)
+                    console.log(event)
+                }
+            }> Aumentar </button>
+        </div>
     )
 
 }
