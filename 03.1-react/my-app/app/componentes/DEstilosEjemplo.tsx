3bd0046799acf90658abde126f272ab94dc40923
Lizbeth Freire <lizbeth.freire@epn.edu.ec>
2023-06-28 14:53:37 -0500

    Estilos, componentes, propiedades
    Definir propiedades dentro de los componentes

--- 03.1-react/my-app/app/componentes/DEstilosEjemplo.tsx
+++ 03.1-react/my-app/app/componentes/DEstilosEjemplo.tsx
@@ -2,10 +2,10 @@
 'use client'
 import styled from "@emotion/styled";
 import { css } from '@emotion/react'
+import cStyles from './c_estilos.module.css'
 
-
-const pinkStyle = css`
-  color: pink;
+const pinkBlue = css`
+  color: orange;
 `
 const TituloNaranja = styled.h1`
   color: orange;
@@ -13,16 +13,29 @@ const TituloNaranja = styled.h1`
 `
 const TituloVerde = styled.h2`
   color: green;
-  font-size: 20px;
+  font-size: 15px;
 `
 export default function DEstilosEjemplo() {
+    const misEstilos = {
+        color: '#fff',
+        backgroundColor: 'black',
+        borderBottom : '5px solid yellow'
+    }
     return(
         <>
             <TituloNaranja>Titulo 1</TituloNaranja>
             <TituloVerde>Titulo 2</TituloVerde>
-            <div css={pinkStyle}>
-                Texto pink
+            <div css = {pinkBlue}>
+                Texto pink (no me vale)
             </div>
+            <p className={cStyles.rojo}>Clase rojo</p>
+            <p style={misEstilos}> Mis Estilos </p>
+            <p style={{
+                color: 'pink',
+                backgroundColor: 'black',
+                borderBottom : '5px solid purple'
+            }
+            }>Estilos en linea</p>
         </>
     )
 }
--- 03.1-react/my-app/app/page.tsx
+++ 03.1-react/my-app/app/page.tsx
@@ -4,7 +4,8 @@ import DEstilosEjemplo from "@/app/componentes/DEstilosEjemplo";
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Hola mundo</h1><DEstilosEjemplo/>
+          <h1>Hola mundo</h1>
+          <DEstilosEjemplo/>
       </main>
   )
 }
